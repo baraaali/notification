ed81a39b6f3538a830cf6597641fa31362f5e967
Rokaya-mk <mbairikrqia@gmail.com>
2021-04-25 23:34:31 +0000

    add device_token and accept_notification to users table

---
+++ database/migrations/2021_04_25_232016_add_divice_token_and_accept_notification.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddDiviceTokenAndAcceptNotification extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {   //add two colums at users table:device_token and accept_notification
+        Schema::table('users', function (Blueprint $table) {
+            //device_token to store tokens of devices to use it for push notification
+            $table->string('device_token')->nullable();
+            //accept_notification for accept or not recieving notification
+            //by default ==1 means notification accepted by user
+            $table->integer('accept_notification')->default(1);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
